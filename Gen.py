3e7b4e19f0b43be297eaa67484c47c4ecef2ec84
hh756 <hh756@tutanota.com>
2022-10-01 13:21:34 -0700

    Changed Varible Names

--- Gen.py
+++ Gen.py
@@ -1,26 +1,25 @@
+import numpy as np
 import hashlib
 numNodes = 4
 x = 0
 y = 0
 hasher = hashlib.md5()
-hss = []
+layer1Hashes = []
 while x < numNodes:
     with open(str(x)+'.txt','rb') as f:
         buf = f.read()
         hasher.update(buf)
         Cnode = hasher.hexdigest() 
-        hss.append(Cnode)
+        layer1Hashes.append(Cnode)
     x += 1
-print(hss)
-hssLen = len(hss)
-hssFirstHalf = hss[:len(hss)//2]
-hssHalf = hssLen/2
-hssLastHalf = []
-while hssHalf > y: 
-    hssfirstHalfplur = hss[int(hssHalf+y)]
+print(layer1Hashes)
+layer1HashesLength = len(layer1Hashes)
+layer1FirstHalf = layer1Hashes[:len(layer1Hashes)//2]
+layer1HalfLength = layer1HashesLength/2
+layer1LastHalf = []
+while layer1HalfLength > y: 
+    layer1LastHalfHash = layer1Hashes[int(layer1HalfLength+y)]
     y += 1
-    hssLastHalf.append(hssfirstHalfplur)
-print(hssFirstHalf)
-print(hssLastHalf)
-
-# Needs to be a function that accepts an Array of interative file names
+    layer1LastHalf.append(layer1LastHalfHash)
+print(layer1LastHalf)
+print(layer1FirstHalf)
